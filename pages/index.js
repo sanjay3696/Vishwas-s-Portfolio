46e5198519425aae9bfcffdd168779e56a914bb2
sanjay3696 <66181363+sanjay3696@users.noreply.github.com>
2022-12-08 21:27:56 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -86,18 +86,17 @@ const Home = () => (
         <Box align="center" my={4}>
           <NextLink href="/works" passHref scroll={false}>
             <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-<div
-  class="DePayButton" 
-  label="Pay"
-  widget="Payment"
-  configuration='{"accept":[{"blockchain":"ethereum","amount":20,"token":"0xa0bEd124a09ac2Bd941b10349d8d224fe3c955eb","receiver":"0x4e260bB2b25EC6F3A59B478fCDe5eD5B8D783B02"},{"blockchain":"bsc","amount":20,"token":"0xa0bEd124a09ac2Bd941b10349d8d224fe3c955eb","receiver":"0x4e260bB2b25EC6F3A59B478fCDe5eD5B8D783B02"}]}'
-/>
-<script src="https://integrate.depay.com/buttons/v8.js"></script>
-<noscript><a href="https://depay.com">Web3 Payments</a> are only supported with JavaScript enabled.</noscript>
-<script>DePayButtons.init({ document });</script>
-        
-              My portfolio
+             My portfolio
             </Button>
+<div
+  class="DePayButton"
+  label="Fghhh"
+  widget="Donation"
+  configuration='{"integration":"70aa5073-7280-40b1-8a85-3d8dc33ba26e","accept":[{"blockchain": "ethereum","token": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE","receiver": "0x11492128a5ef0b07822214eff4ec02629b9b0760"},{"blockchain": "ethereum","token": "0xdAC17F958D2ee523a2206206994597C13D831ec7","receiver": "0x11492128a5ef0b07822214eff4ec02629b9b0760"}]}'
+></div>
+<script src="https://integrate.depay.com/buttons/v7.js"></script>
+<noscript><a href="https://depay.com">Web3 Payments</a> are currently only supported with JavaScript enabled.</noscript>
+<script>DePayButtons.init({document: document});</script>
           </NextLink>
         </Box>
       </Section>
